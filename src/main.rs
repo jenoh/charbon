715ce51d620fc8a4de409b9675fd25e43b9ff15a
jenoh <memint.contact@gmail.com>
2022-11-25 12:14:08 +0100

    Add config file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use log::info;
+use std::error::Error;
+
+mod config;
+
+use config::config::Config;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+    let mut config = Config::default();
+    config.get_config();
+    info!("Config tick_rate: {}", config.get_tick_rate());
+    Ok(())
 }
