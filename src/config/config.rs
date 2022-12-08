fadc356ec1ac486ec9f09dbb0b9a4eaa76ce9b95
jenoh <memint.contact@gmail.com>
2022-12-08 13:57:44 +0100

    add dynamic layout from config.yaml

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,15 +1,16 @@
 use serde::Deserialize;
+use tui::layout::Constraint;
 
 #[derive(Default, Debug, Deserialize)]
 pub struct Config {
-    tick_rate: i32,
+    tick_rate: u32,
     layout: Vec<Layout>,
 }
 
 #[derive(Default, Debug, Deserialize)]
 pub struct Layout {
     app_name: String,
-    size: i8,
+    size: u16,
 }
 
 impl Config {
@@ -21,6 +22,19 @@ impl Config {
         let f = std::fs::File::open(path_config_file).expect("Could not open file.");
         self.set_config(serde_yaml::from_reader(f).expect("Could not read values."));
     }
+    /*
+     * Converte layout size to Vec<Constraint>
+     */
+    pub fn get_contraints(&mut self) -> Vec<Constraint> {
+        let mut vec: Vec<Constraint> = Vec::new();
+        for l in &self.layout {
+            vec.push(Constraint::Percentage(l.size))
+        }
+        return vec;
+    }
+    /*
+     * Setters
+     */
     fn set_config(&mut self, config: Config) {
         self.tick_rate = config.tick_rate;
         self.layout = config.layout;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,70 @@
-use config::config::Config;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders},
+    Frame, Terminal,
+};
+
+use config::config::Config;
+
 mod config;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // get config
     let mut config = Config::default();
     config.get_config();
     println!("{:?}", config);
+    let c = config.get_contraints();
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let res = run_app(&mut terminal, c);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
     Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, c: Vec<Constraint>) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, c.clone()))?;
+        if let Event::Key(key) = event::read()? {
+            if let KeyCode::Char('q') = key.code {
+                return Ok(());
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, c: Vec<Constraint>) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(c.as_ref())
+        .split(f.size());
+
+    let block = Block::default().title("Block").borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+    let block = Block::default().title("Block 2").borders(Borders::ALL);
+    f.render_widget(block, chunks[1]);
 }
