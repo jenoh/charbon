6c5df395bac56378a7d0009b1f4e324e0ba42339
jenoh <memint.contact@gmail.com>
2022-12-16 16:05:15 +0100

    Add impl Widget on render function

--- src/features/features.rs
+++ src/features/features.rs
@@ -1,14 +1,15 @@
-use tui::widgets::Table;
-extern crate strum;
 use crate::features::system_info;
+use tui::widgets::Widget;
+
+extern crate strum;
 
 #[derive(EnumString)]
 pub enum Features {
     System,
 }
 
-pub fn select_feature(choice: Features) -> Table<'static> {
-    match choice {
+pub fn select_feature(selected: Features) -> impl Widget {
+    match selected {
         Features::System => system_info::SystemInfo::render(),
     }
 }
